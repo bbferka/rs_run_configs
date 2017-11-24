6c301d5a9096543e77fca821cc5239e3ab0bc16c
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2017-11-24 22:54:15 +0100

    launch file to start RS with web frontend

---
+++ prolog/init.pl
@@ -0,0 +1,5 @@
+:- register_ros_package(rs_prolog_interface).
+:- register_ros_package(knowrob_vis).
+:- register_ros_package(knowrob_mongo).
+
+:- owl_parser:owl_parse('package://iai_semantic_maps/owl/kitchen.owl').
